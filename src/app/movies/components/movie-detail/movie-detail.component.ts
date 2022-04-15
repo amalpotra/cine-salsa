73f0092755b654ea08e061ddb1323ffa68470073
Amarjeet Malpotra <amalpotra@catalystone.com>
2022-04-15 12:43:48 +0530

    Updated movie detail component
    - Added backdrop image in the background
    - Fetch genres just once and that too in movie-details component only
    - Genres in place

--- src/app/movies/components/movie-detail/movie-detail.component.ts
+++ src/app/movies/components/movie-detail/movie-detail.component.ts
@@ -1,23 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import Movie from '../../models/movie';
 import { MovieService } from '../../services/movie/movie.service';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { environment } from 'src/environments/environment';
+import { faArrowLeft, faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
+import Movie from '../../models/movie';
+import MovieGenre from '../../models/movieGenre';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss'],
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit {
   faArrowLeft = faArrowLeft;
+  faArrowsRotate = faArrowsRotate;
 
   constructor(
     private movieService: MovieService,
     private route: ActivatedRoute
   ) {}
 
+  ngOnInit(): void {
+    !this.movieService.getMovieGenres().length &&
+      this.movieService.fetchMovieGenres();
+  }
+
+  isFetching(): boolean {
+    return this.movieService.isFetching();
+  }
+
   getMovie(): Movie | undefined {
     return this.movieService.getMovieBySlug(this.route.snapshot.url[0].path);
+  }
+
+  backdropBuilder(backdropPath: string): string {
+    return `linear-gradient(rgba(0,0,0,0.7) , rgba(0,0,0,0.7)), url(${environment.apiImageURL}original${backdropPath})`;
+  }
+
+  getGenresByIds(genreIds: number[]) {
+    return genreIds.map(
+      (genreId: number) =>
+        this.movieService
+          .getMovieGenres()
+          .find((genre: MovieGenre) => genreId === genre.id)?.name
+    );
   }
 }
